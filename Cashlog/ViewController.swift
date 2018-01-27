f98eee260967465000074c4cb2fca815e912f7c2
Bruce <t15thbruce@outlook.de>
2018-01-27 13:25:04 +0100

    Initial Commit

---
+++ Cashlog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cashlog
+//
+//  Created by Bruce Röttgers on 27.01.18.
+//  Copyright © 2018 Bruce Röttgers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
